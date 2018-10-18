c7b172cd8cd47bbcb9d1e46bbf0cf539cd2075c1
Phan Khanh <kimtuthanhcf@gmail.com>
2018-10-18 21:06:10 +0700

    failed

--- appread/app.js
+++ appread/app.js
@@ -86,64 +86,6 @@ app.get('/register',function(req,res){
     res.render('register');
 	});
 
-// Register User
-// app.post('/register', function (req, res) {
-// 	var name = req.body.name;
-// 	var email = req.body.email;
-// 	var username = req.body.username;
-// 	var PhoneNumber = req.body.PhoneNumber;
-// 	var password = req.body.password;
-// 	var password2 = req.body.password2;
-
-// 	// Validation
-// 	req.checkBody('name', 'Name is required').notEmpty();
-// 	req.checkBody('email', 'Email is required').notEmpty();
-// 	req.checkBody('email', 'Email is not valid').isEmail();
-// 	req.checkBody('username', 'Username is required').notEmpty();
-// 	req.checkBody('PhoneNumber','PhoneNumber is required').notEmpty();
-// 	req.checkBody('password', 'Password is required').notEmpty();
-// 	req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
-
-// 	var errors = req.validationErrors();
-
-// 	if (errors) {
-// 		res.render('register', {
-// 			errors: errors
-// 		});
-//     }else {
-		
-// 		//checking for email and username are already taken
-// 		User.findOne({ username: { 
-// 			"$regex": "^" + username + "\\b", "$options": "i"
-// 	    }}, function (err, user) {
-// 			User.findOne({ email: { 
-// 				"$regex": "^" + email + "\\b", "$options": "i"
-// 		}}, function (err, mail) {
-// 				if (user || mail) {
-// 					res.render('register', {
-// 						user: user,
-// 						mail: mail
-// 					});
-// 				}
-// 				else {
-// 					var newUser = new User({
-// 						name: name,
-// 						email: email,
-// 						username: username,
-// 						password: password
-// 					});
-// 					User.createUser(newUser, function (err, user) {
-// 						if (err) throw err;
-// 						console.log(user);
-// 					});
-//          	req.flash('success_msg', 'You are registered and can now login');
-// 					res.redirect('/login');
-// 				}
-// 			});
-// 		});
-// 	}
-    
-// });
 //Nexmo
 
 const nexmo = new Nexmo({
@@ -177,7 +119,7 @@ password1 = password;
 	req.checkBody('password', 'Password is required').notEmpty();
 	req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
 	var errors = req.validationErrors();
-
+console.log(errors)
 	if (errors) {
 		res.render('register', {
 			errors: errors
